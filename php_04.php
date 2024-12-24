1c4d8360d2a1898cb84910d14f5fe4099067913e
tawan's laptop <43491902+tawan475@users.noreply.github.com>
2024-12-24 18:09:32 +0700

    feat: add PHP files for multiplication and number classification with Bootstrap styling

---
+++ php_04.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta name="author" content="66160084">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <?php
+        function gen(int $start, int $end): void
+        {
+            for ($i = $start; $i <= $end; $i++) {
+                echo "<p class='m-0'>";
+                echo $i . " = ";
+                if ($i % 2 == 0) {
+                    echo "เลขคู่";
+                } else {
+                    echo "เลขคี่";
+                }
+                echo "</p>";
+            }
+        }
+        ?>
+        <h1>Workshop #HTML - PHP</h1>
+        <div class="container">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="startInput" class="form-label">Start</label>
+                    <input type="number" class="form-control" id="startInput" name="startInput">
+                </div>
+                <div class="mb-3">
+                    <label for="endInput" class="form-label">End</label>
+                    <input type="number" class="form-control" id="endInput" name="endInput">
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Generate</button>
+            </form>
+
+            <?php
+            if (isset($_POST['submit']) && !empty($_POST['startInput']) && !empty($_POST['endInput'])) {
+                gen($_POST['startInput'], $_POST['endInput']);
+            }
+            ?>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
