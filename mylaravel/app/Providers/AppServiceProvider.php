cfc6be9b76f28b07afb554e3c59f3d0dfb1abe6a
tawan's laptop <43491902+tawan475@users.noreply.github.com>
2025-02-26 23:10:59 +0700

    fix: update URL scheme enforcement to check for HTTPS in environment configuration

--- mylaravel/app/Providers/AppServiceProvider.php
+++ mylaravel/app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if(env('APP_ENV') !== 'local') {
+        if(env('APP_SCHEMA') === 'https') {
             URL::forceScheme('https');
         }
 
