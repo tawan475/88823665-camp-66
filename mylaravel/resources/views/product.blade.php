f58448054aec2474fea928bacd32f9932fac33bb
tawan's laptop <43491902+tawan475@users.noreply.github.com>
2025-02-26 23:22:18 +0700

    fix: update AdminLTE script path to use public assets and remove unused lockscreen.html

--- mylaravel/resources/views/product.blade.php
+++ mylaravel/resources/views/product.blade.php
@@ -764,7 +764,7 @@
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
     <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="../../../dist/js/adminlte.js"></script>
+    <script src="{{ url('public/assets/js/adminlte.js') }}"></script>
     <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
     <script>
         const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
