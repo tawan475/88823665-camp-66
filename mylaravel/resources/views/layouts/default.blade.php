54795a2784ab2bb9b45d0f6800be2cc8c00935cd
tawan's laptop <43491902+tawan475@users.noreply.github.com>
2025-02-26 23:19:50 +0700

    fix: update script path for AdminLTE and add minified version

--- mylaravel/resources/views/layouts/default.blade.php
+++ mylaravel/resources/views/layouts/default.blade.php
@@ -58,7 +58,7 @@
       crossorigin="anonymous"
     ></script>
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
-    <script src="{{ url('public/js/adminlte.js') }}"></script>
+    <script src="{{ url('public/assets/js/adminlte.js') }}"></script>
     <script>
       const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
       const Default = {
